c7f0f932a47e5d983390f4bc2891215577b93b6b
woliveiras <w.oliveira542@gmail.com>
2018-04-18 07:07:56 -0300

    Add file-loader

--- webapp/src/index.jsx
+++ webapp/src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
+import './img/favicon.ico'
 
 render(
   <h1>Welcome to React!</h1>,
--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -31,6 +31,10 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.(jpe?g|ico|png|gif|eot|woff|woff2|ttf|svg)$/i,
+        loader: 'file-loader?name=img/[name].[ext]'
       }
     ]
   },
