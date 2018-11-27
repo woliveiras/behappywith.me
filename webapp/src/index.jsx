a1d85e1e401cd596b97d25220ee538b578b90540
woliveiras <w.oliveira542@gmail.com>
2018-11-27 06:56:36 -0200

    Add desktop install

--- webapp/src/index.jsx
+++ webapp/src/index.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 import './img/favicon.ico'
+import './img/favicon.ico';
+import './img/icon-72x72.png';
+import './img/icon-96x96.png';
+import './img/icon-128x128.png';
+import './img/icon-144x144.png';
+import './img/icon-152x152.png';
+import './img/icon-192x192.png';
+import './img/icon-384x384.png';
+import './img/icon-512x512.png';
 import './index.css'
 import './css/pure-min.css'
 import { render } from 'react-dom'
